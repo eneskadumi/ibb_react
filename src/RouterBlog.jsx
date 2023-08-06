77ae32bcf9737dd00ea6ee75e204e249b2530e5c
eneskadumi <eneskadumi@gmail.com>
2023-08-06 14:08:51 +0300

    RouterBlog

---
+++ src/RouterBlog.jsx
@@ -0,0 +1,31 @@
+// rcc
+
+import React, { Component } from 'react'
+
+
+// CLASS
+export default class RouterBlog extends Component {
+
+    // constructor
+    constructor(props) {
+        super(props);
+        // state
+        this.state = {};
+        // bind
+
+    }
+
+    // CDM
+
+    // Function
+
+
+
+    // Render
+    render() {
+        return (
+            <div>RouterBlog</div>
+        ) // end return
+    } //end render()
+}
+//end class
