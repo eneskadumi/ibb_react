4c94644e2f8b7718d1145c7f4c53d1e0ce73ce83
eneskadumi <eneskadumi@gmail.com>
2023-08-12 14:56:46 +0300

    BlogList

--- src/component/blog/BlogList.jsx
+++ src/component/blog/BlogList.jsx
@@ -40,8 +40,31 @@ class BlogList extends Component {
 
 
   render() {
+    const {t} = this.props;
+
+    
     return (
-      <div>BlogList</div>
+      <React.Fragment>
+        <h1 className="text-center display-4 text-uppercase">Blog List</h1>
+      <table className= "table table-hover table-striped">
+        <thead>
+          <tr>
+            <th>{t('blog_id')}</th>
+            <th>{t('blog_header')}</th>
+            <th>{t('blog_content')}</th>
+            <th>{t('date')}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>item 1</td>
+            <td>item 2</td>
+            <td>item 3</td>
+            <td>item 4</td>
+          </tr>
+        </tbody>
+      </table>
+      </React.Fragment>
     )
   }
 }
